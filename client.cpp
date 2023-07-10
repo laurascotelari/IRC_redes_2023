9cb62f0f50e09c1de981b2bb85784b4059cfc520
laurascotelari <laura.scotelari@usp.br>
2023-07-10 15:32:04 -0300

    Criando arquivos de servidor e cliente

---
+++ client.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <string>
+
+#include <cstdlib>  // usar rand() e srand()
+#include <ctime>  
+
+using namespace std;
+
+#define MAX_MSG 4096
+
+string generateNickname(string nickname, int flag){
+    srand(time(0));
+    
+    //gera um nickname aleatorio: cliente + numero aleatorio
+    if(flag){
+        return nickname;
+    }else{
+        //pegando um numero aleatrio entre 0 e 100
+        int randNum = rand()%100;
+
+        string newNickname;
+        newNickname = "cliente" + to_string(randNum);
+
+        return newNickname;
+    }
+}
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		cout << "use: ./client <IPaddress>";
+        return -1;
+    }
+
+    /** 
+     * Cria um socket para o cliente
+     * // AF_INET diz que vamos usar IPv4
+     * // SOCK_STREAM diz que vamos usar um stream confiável de bytes
+     * // IPPROTO_TCP diz que vamos usar TCP. Poderíamos passar zero em 
+     *   seu lugar, já que SOCK_STREAM por padrão usarái TCP.
+    */
+    int client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    /**
+     * A struct sockaddr_in server_address vai armazenar as informações
+     * do socket a que iremos nos conectar. No caso, nos conectaremos ao
+     * localhost (127.0.0.1) na porta 8080 (o servidor deve estar escutando)
+     * nesta porta.
+     */
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET; // IPv4
+    server_address.sin_port = htons(8080); // Porta 8080
+    inet_pton(AF_INET, argv[1], &server_address.sin_addr); // Associação com IP do servidor
+
+    /**
+     * Vamos agora de fato nos conectar ao servidor. Passamos os nosso socket que criamos (veja que não
+     * especificamos em que porta estamos escutando, ou seja, não utilizamos bind, porque nós, como clientes
+     * não nos importamos com isso. Temos nosso IP, independentemente de a partir de que porta, queremos nos)
+     * conectar ao servidor num endereço IP e porta específica. No caso, sabemos que o servidor ao qual queremos
+     * nos conector está rodando no endereço "127.0.0.1" escutando na porta 8080). Passamos em seguida as 
+     * informações de endereço do socket do servidor, que configuramos acima. Por fim, o tamanho desta estrutura
+     * do socket do servidor (por algum motivo, o C++ precisa disso). 
+     */
+    connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+
+    /**
+     * Uma vez que a conexão foi estabelecida a conexão, informamos isso com uma mensagem no terminal.
+     */
+    cout << "Você está conectado ao servidor!" << endl;
+
+    /**
+     * Com a conexão estabelecida, 3-way handshake do TCP e tudo mais, podemos começar a troca de mensagens
+     * através do uso das primitivas de socket 'send' e 'recv'.
+     * 
+     * Precisamos primeiro configurar o buffer que vai receber as mensagens com um tamanho arbitrário de 1024.
+     * As mensagens vão sendo recebidas neste buffer, o qual podemos consumir para ler.
+     */
+    char buffer[MAX_MSG] = {0};
+    string mensagemLida;
+
+    //mensagem ja formatada: apelido + mensagem
+    string mensagem;
+
+    string nickname = generateNickname(mensagem, 0);
+
+    while (true) {
+        // Ler entrada do usuário para mandar para o servidor.
+        cout << "\nEscreva sua mensagem: ";
+        getline(cin, mensagemLida);
+
+        //formatando a mensagem do usuario adicionando o seu nickname
+        mensagem = nickname + ": " + mensagemLida; 
+
+        cout << "\n" << mensagem << "\n\n";  
+
+        // Mandar mensagem ao servidor.
+        send(client_socket, mensagem.c_str(), mensagem.length(), 0);
+
+        // Checar se o usuário quer sair caso a mensagem enviada ao servidor tenha sido o comando '/exit'.
+        if (mensagemLida == "/quit") break;
+
+        // Receber resposta do servidor.
+        recv(client_socket, buffer, 1024, 0);
+        cout << "Mensagem do Servidor: " << buffer << endl;
+
+        // Limpar buffer de recepção para se preparar para próxima mensagem
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    // Fechar o socket
+    close(client_socket);
+
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
+using namespace std;
+
+#define MAX_MSG 4096
+
+int main() {
+    /** 
+     * Cria um socket para o servidor
+     * // AF_INET diz que vamos usar IPv4
+     * // SOCK_STREAM diz que vamos usar um stream confiável de bytes (TCP)
+     * // IPPROTO_TCP diz que vamos usar TCP. Poderíamos passar zero em 
+     *   seu lugar, já que SOCK_STREAM por padrão usarái TCP.
+    */
+    int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    /**
+     * A struct sockaddr_in server_address vai armazenar as informações
+     * do socket que estamos criando para o servidor. No caso, vamos ser um
+     * servidor que roda no localhost (127.0.0.1) e escuta na porta 8080.
+     */
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(8080);
+
+    /**
+     * Como somos um servidor, vamos utilizar a primitiva de socket 'bind' para 
+     * atrelar nosso socket com as informações de nos endereço e porta específicos,
+     * no caso IP '127.0.0.1' e porta '8080'. 
+     */
+    bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+
+    /**
+     * Vamos agora explicitamente escutar e aguardar por conexões.
+     * O 3 indica conseguimos receber até 3 conexões que ficam aguardando em uma fila.
+     */
+    listen(server_socket, 3);
+
+    /**
+     * Acima dissemos que vamos escutar por conexões. Agora precisamos aceitar estas 
+     * conexões para poder lidar com cada uma delas.
+     */
+    int client_socket;
+    struct sockaddr_in client_address;
+    socklen_t addrlen = sizeof(client_address);
+    client_socket = accept(server_socket, (struct sockaddr *)&client_address, &addrlen);
+
+    /**
+     * Estabelecida a conexão, podemos informar isto pelo terminal. 
+     */
+    cout << "Cliente esta conectado!" << endl;
+
+    /**
+     * Com a conexão estabelecida, 3-way handshake do TCP e tudo mais, podemos começar a troca de mensagens
+     * através do uso das primitivas de socket 'send' e 'recv'.
+     * 
+     * Precisamos primeiro configurar o buffer que vai receber as mensagens com um tamanho arbitrário de 1024.
+     * As mensagens vão sendo recebidas neste buffer, o qual podemos consumir para ler.
+     */
+    char buffer[MAX_MSG] = {0};
+    string message;
+
+    while (true) {
+        // Primeiro recebemos a mensagem que o cliente nos enviou.
+        recv(client_socket, buffer, 1024, 0);
+
+        //printando na tela a mensagem enviada pelo cliente
+        cout << buffer << endl;
+
+        // Lida e impressa a mensagem, podemos limpar o buffer para preparar para novas mensagens.
+        memset(buffer, 0, sizeof(buffer));
+
+        // Checar se o cliente quer fechar a conexão com comando '/exit'.
+        if (message == "/quit") break;
+
+        // Ler entrada do usuário para mandar para o cliente.
+        cout << "Escreva sua mensagem: ";
+        getline(cin, message);
+
+        // Mandar mensagem para o cliente.
+        send(client_socket, message.c_str(), message.length(), 0);
+    }
+
+    // Fechar os sockets
+    close(client_socket);
+    close(server_socket);
+
+    return 0;
+}
